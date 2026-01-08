7259b1450621a03c496921a506bf2fee430e9973
yaarifri <yaarifri@gmail.com>
2026-01-08 21:45:41 +0200

    Initial commit

---
+++ src/main/java/net/yaar1ph/startmod/StartMod.java
@@ -0,0 +1,20 @@
+package net.yaar1ph.startmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StartMod implements ModInitializer {
+	public static final String MOD_ID = "startmod";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/yaar1ph/startmod/StartModClient.java
@@ -0,0 +1,10 @@
+package net.yaar1ph.startmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StartModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
