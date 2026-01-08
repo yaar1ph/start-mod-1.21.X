f6beeeadd63b26932dd2be069daaecd08d001459
yaarifri <yaarifri@gmail.com>
2026-01-08 21:50:39 +0200

    X

---
+++ src/main/java/net/yaar1ph/startmod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.yaar1ph.startmod.item;
+
+import net.yaar1ph.startmod.StartMod;
+
+public class ModItems {
+
+    public static  void registerModItems() {
+        StartMod.LOGGER.info("Registering Mod Items for " + StartMod.MOD_ID);
+    }
+}
