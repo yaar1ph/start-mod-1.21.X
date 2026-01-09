531dd6c6a24ea758c1d8e1fb6c0df9e3d7c65402
yaarifri <yaarifri@gmail.com>
2026-01-09 08:01:09 +0200

    X

--- src/main/java/net/yaar1ph/startmod/item/ModItems.java
+++ src/main/java/net/yaar1ph/startmod/item/ModItems.java
@@ -1,22 +1,29 @@
 package net.yaar1ph.startmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.yaar1ph.startmod.StartMod;
 
 public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
+    public static final RegistryKey<Item> PINK_GARNET_KEY =
+            RegistryKey.of(RegistryKeys.ITEM,
+                    Identifier.of(StartMod.MOD_ID, "pink_garnet"));
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(StartMod.MOD_ID, name), item);
+    public static final Item PINK_GARNET = Registry.register(
+            Registries.ITEM,
+            PINK_GARNET_KEY,
+            new Item(new Item.Settings().registryKey(PINK_GARNET_KEY))
+    );
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register(entries -> entries.add(PINK_GARNET));
     }
-
-
-    public static  void registerModItems() {
-        StartMod.LOGGER.info("Registering Mod Items for " + StartMod.MOD_ID);
-    }
-
 }
